3de260bf28c9644024a900b708142d7b0aa26cfd
renzo <github@renzokuken.eu>
2016-08-23 15:48:28 +0200

    remove deb mult

--- debian.fete.sh
+++ debian.fete.sh
@@ -1,7 +1,4 @@
-cp sources.list_sid_multimedia /etc/apt/sources.list
-
 bash ./debian.desktop.sh
 
-apt-get install deb-multimedia-keyring
 
 apt-get install banshee clementine deadbeef ffmpeg firmware-linux-free firmware-linux-nonfree firmware-misc-nonfree gimp rhythmbox rtmpdump ruby-sequel ruby-sinatra ruby-sinatra-contrib ruby-slim sqlite3 sshfs youtube-dl
